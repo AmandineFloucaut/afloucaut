dad30b3cdf74e6be36202ed8b454a776b9aaf99b
AmandineFloucaut <afloucaut@gmail.com>
2021-06-12 16:53:54 +0200

    responsive header ok

--- src/components/HeaderCv.vue
+++ src/components/HeaderCv.vue
@@ -47,9 +47,8 @@ export default {
 
 .header-cv {
     display: flex;
-    justify-content: space-between;
-    // height: 20%;
-    padding: 1rem 1rem 0 1rem;
+    justify-content: center;
+    padding: 1rem 0.5rem 0 0.5rem;
 
     background-image: url("~@/assets/images/image_fond_nuage.png");
     background-size: cover;
@@ -60,25 +59,23 @@ export default {
 
     .info {
 
-        flex-grow: 1;
-
         &__job {
             font-family: 'Lobster';
-            font-weight: 700;
-            font-size: 1rem;
+            font-weight: 400;
+            font-size: 1.2rem;
             margin: 1rem 0 0.5rem 0;
         }
 
         &__job2 {
             font-family: 'Matiz';
             font-weight: 900;
-            font-size: 0.6rem;
+            font-size: 0.8rem;
 
         }
 
         &__stack {
             font-family: 'AbrahamLincoln';
-            font-size: 0.7rem;
+            font-size: 0.8rem;
             margin-top: 1rem;
         }
 
@@ -90,8 +87,8 @@ export default {
 
         &__location {
             font-family: 'MrDafoe';
-            font-size: 0.5rem;
-            margin-bottom: 0.5rem;
+            font-size: 0.6rem;
+            // margin-bottom: 0.5rem;
         }
     }
 
@@ -128,8 +125,8 @@ export default {
 
         &__heart {
             width: 80%;
-            top: 20px;
-            right: -10px;
+            top: 25px;
+            right: -7px;
 
             &--image {
                 width: 100%;
@@ -138,7 +135,7 @@ export default {
         }
 
         &__age {
-            width: 100%;
+            // width: 100%;
             bottom: 50px;
             right: 10px;
 
@@ -151,5 +148,274 @@ export default {
 }
 
 //TODO Medias queries
+
+@media(min-width: 360px) {
+
+    .header-cv {
+
+        .bubble {
+
+            &__name {
+                left: -40px;
+            }
+
+            &__heart {
+                top: 50px;
+                right: -5px;
+            }
+        }
+    }
+}
+
+@media(min-width: 400px) {
+
+    .header-cv {
+
+        .photo-cv{
+            width: 40%;
+            height: auto;
+        }
+
+        .bubble {
+
+            &__name {
+                width: 90%;
+                left: -30px;
+            }
+
+            &__heart {
+                width: 60%;
+                top: 45px;
+                right: -10px;;
+            }
+
+            &__age {
+                width: 70%;
+                top: 100px;
+            }
+        }
+
+        .info {
+
+            &__job {
+                margin-top: 1.5rem;
+                font-size: 1.5rem;
+            }
+
+            &__job2 {
+                font-size: 0.8rem;
+            }
+
+            &__stack {
+                font-size: 0.8rem;
+            }
+
+            &__name {
+                margin-top: 1rem;
+                font-size: 0.9rem;
+            }
+
+            &__location {
+                font-size: 0.7rem;
+            }
+        }
+    }
+}
+
+@include screen-medium {
+
+    .header-cv {
+
+        .info {
+
+            &__job {
+                font-size: 2rem;
+            }
+
+            &__job2 {
+                font-size: 2.5rem;
+            }
+
+            &__stack {
+                font-size: 2rem;
+            }
+
+            &__name {
+                font-size: 1.5rem;
+                margin-top: 2rem;
+            }
+
+            &__location {
+                font-size: 1rem;
+            }
+        }
+
+        .photo-cv{
+            width: 35%;
+            height: auto;
+        }
+
+        .bubble {
+
+            &__name {
+                width: 70%;
+                top: 20px;
+                left: -40px;
+            }
+
+            &__heart {
+                width: 40%;
+                top: 55px;
+                right: 10px;
+            }
+
+            &__age {
+                width: 60%;
+                top: 120px;
+                right: 50px;
+            }
+        }
+    }
+}
+
+@include screen-large {
+
+    .header-cv {
+
+        .photo-cv {
+            width: 25%;
+            height: auto;
+        }
+
+        .bubble {
+
+            &__name {
+                width: 60%;
+                top: 20px;
+                left: -20px;
+            }
+
+            &__heart {
+                width: 30%;
+                top: 55px;
+                right: 10px;
+            }
+
+            &__age {
+                width: 50%;
+                top: 120px;
+                right: 50px;
+            }
+        }
+    }
+
+}
+
+@include screen-extra-large {
+
+    .header-cv {
+
+        justify-content: space-around;
+
+          .info {
+
+            &__job {
+                font-size: 3rem;
+                margin-top: 3rem;
+            }
+
+            &__job2 {
+                font-size: 3.5rem;
+            }
+
+            &__stack {
+                font-size: 2.5rem;
+            }
+
+            &__name {
+                font-size: 2rem;
+                margin-top: 3rem;
+            }
+
+            &__location {
+                font-size: 1.5rem;
+            }
+        }
+
+        .photo-cv {
+            width: 30%;
+            height: auto;
+        }
+
+        .bubble {
+
+            &__name {
+                width: 70%;
+                top: 20px;
+                left: -80px;
+            }
+
+            &__heart {
+                width: 40%;
+                top: 70px;
+                right: 50px;
+            }
+
+            &__age {
+                width: 60%;
+                top: 180px;
+                right: 100px;
+            }
+        }
+    }
+}
+
+@media(min-width: 1500px) {
+
+    .header-cv {
+
+        .info {
+
+            &__job {
+                margin-top: 8rem;
+            }
+
+            &__name {
+                margin-top: 4rem;
+            }
+        }
+
+        .bubble {
+
+            &__name {
+                width: 65%;
+                top: 20px;
+                left: -150px;
+            }
+
+            &__heart {
+                width: 35%;
+                top: 90px;
+                right: 100px;
+            }
+
+            &__age {
+                width: 55%;
+                top: 250px;
+                right: 200px;
+            }
+        }
+    }
+}
+
+@include screen-huge {
+
+    .header-cv {
+
+        .bubble {
+            margin-top: 4rem;
+        }
+    }
+}
 
 </style>
